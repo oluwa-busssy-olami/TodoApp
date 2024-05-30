ae852cf4f4e5be16f572c6e99a9a921f91166b59
Oluwanifesimi-Kenny <busolamikenny@gmail.com>
2024-05-30 13:48:37 +0100

    todo-update

--- todo-list/src/Component/Todolist/TodoApp.jsx
+++ todo-list/src/Component/Todolist/TodoApp.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Todolist.css";
 import FormSection from "./FormSection";
 import TodoList from "./TodoList";
-import Buttons from "./Buttons";
-import TodoItem from "./TodoItem";
 
 const TodoApp = () => {
+  const initilTasks = [
+    {
+      id: 1,
+      name: "cook",
+    },
+    {
+      id: 2,
+      name: "cook",
+    },
+  ];
+
+  const [tasks, setTasks] = useState(initilTasks);
+  const addNewTask = (task) => {
+    // console.log(`In the TodoApp The new task is ${task}`);
+    const taskObject = {
+      id: 3,
+      name: `${task}`,
+    };
+    const updatedTasks = [taskObject, ...tasks];
+    // console.log(...tasks);
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className="container">
-      <FormSection />
-      <TodoList />
-      {/* <TodoItem />
-      <Buttons /> */}
+      <FormSection handleClick={addNewTask} />
+      <TodoList tasks={tasks} />
     </div>
   );
 };
--- todo-list/src/Component/Todolist/TodoList.jsx
+++ todo-list/src/Component/Todolist/TodoList.jsx
@@ -1,33 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 // import green_tick from "../../assests/green-tick.jpg";
 import remove_icon from "../../assests/delete.jpg";
 
 // Data Sourc
-const tasks = [
-  {
-    id: 1,
-    name: "cook",
-  },
-  {
-    id: 2,
-    name: "cook",
-  },
-  {
-    id: 3,
-    name: "cook",
-  },
-  {
-    id: 4,
-    name: "cook",
-  },
-];
-const TodoList = () => {
-  const addTask = tasks.map((task, index) => {
+
+const TodoList = (props) => {
+  const handleDelClick = () => {
+    console.log("delete");
+  };
+  const [toggleColour, setToggleColor] = useState("white");
+  const handleToggle = () => {
+    setToggleColor(toggleColour);
+    console.log("toggle");
+  };
+  const addTask = props.tasks.map((task, index) => {
     return (
       <div key={task.id} className="task">
         <div className="check-box">
-          <button>
-            toggle
+          <button
+            className="toggle"
+            onClick={handleToggle}
+            style={{ backgroundColor: toggleColour }}>
             {/* <img src={green_tick} alt="" className="check-click" /> */}
           </button>
           <span className="taskName">{task.name}</span>
@@ -35,7 +28,7 @@ const TodoList = () => {
 
         <div className="btn-task">
           {/* <button className="edit-btn">edit</button> */}
-          <button className="del-btn">
+          <button className="del-btn" onClick={handleDelClick}>
             <img src={remove_icon} alt="" className="del" />
           </button>
         </div>
@@ -44,7 +37,9 @@ const TodoList = () => {
   });
   return (
     <div className="todo-task">
-      <h3>TASKS</h3>
+      <h3>
+        <i>Todo-item</i>
+      </h3>
       {addTask}
     </div>
   );
